3b6c97329b77b85c09082843510913c757d389cf
luizsilva <luizfsilva.091595@gmail.com>
2021-07-13 17:22:04 -0400

    connected to Firebase and created get all events

--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-const { response } = require('express')
+const { response, request } = require('express')
 const firebase = require('firebase-admin')
 const credentials = require('../credentials.json')
 
