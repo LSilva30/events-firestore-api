9c0173a258eec10f9f9cd6be23bd525ad3012682
luizsilva <luizfsilva.091595@gmail.com>
2021-07-15 08:58:14 -0400

    added a post, get, delete, patch and search feature

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const cors = require('cors')
-const { getEvents , addEvent } = require('./src/events')
+const { getEvents , addEvent, getEventById, deleteEvent, updateEvent, eventSearch } = require('./src/events')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/events/search', eventSearch)
+app.get('/events/:eventId', getEventById)
 app.get('/events', getEvents)
 
 app.post('/events', addEvent)
+
+app.delete('/events/:eventId', deleteEvent)
+
+app.patch('/events/:eventId', updateEvent)
 
 app.listen(5000, () => {
     console.log('Connected on port 5000')
--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-const { response, request } = require('express')
+const { request, response } = require('express')
 const firebase = require('firebase-admin')
 const credentials = require('../credentials.json')
 
@@ -13,13 +13,14 @@ function connectDb() {
 
 exports.getEvents = (req, res) => {
   const db = connectDb()
-  db.collection('events')
-    .get()
-    .then(allEvents => {
-      allEvents.docs.map(doc => {
-        console.log(doc.data())
+  db.collection('events').get()
+    .then(eventsCollection => {
+     const allEvents = eventsCollection.docs.map(doc => {
+        let events = doc.data()
+        events.id = doc.id
+        return events
       })
-      res.send('got all events')
+      res.send(allEvents)
     })
     .catch(err => console.log(err))
 }
@@ -32,4 +33,52 @@ exports.addEvent = (req, res) => {
     .add(req.body)
     .then(doc => res.send(doc))
     .catch(err => res.status(500).send('Event could not be created'))
+}
+
+// create GET - ONE event request
+
+exports.getEventById = (req, res) => {
+  const db = connectDb()
+  db.collection('events').doc(req.params.eventId).get()
+  .then(eventDoc => {
+    const event = eventDoc.data()
+    event.id = eventDoc.id
+    res.send(event)
+  })
+  .catch(err => res.status(500).send('Could not find event by Id'))
+}
+
+// create DELETE request - to delete one event 
+
+exports.deleteEvent = (req, res) => {
+const db = connectDb()
+const { eventId } = req.params
+db.collection('events').doc(eventId).delete()
+.then(() => res.status(200).send('Event was deleted.'))
+.catch(err => res.status(500).send('Could not delete event by Id'))
+}
+
+// create PATCH request - to update one event
+exports.updateEvent = (req, res) => {
+const db = connectDb()
+const { eventId } = req.params
+db.collection('events').doc(eventId).update(req.body)
+.then(res.status(200).send('Event was updated succesfully!'))
+.catch(err => res.status(500).send('Could not update event!'))
+}
+
+// make a SEARCH request - to find one event
+exports.eventSearch = (req, res) => {
+  const { eventName } = req.query
+  const db = connectDb()
+  db.collection('events').where('name', '==', eventName).get()
+  .then(eventsCollection => {
+    const matches = eventsCollection.docs.map(doc => {
+      let event = doc.data()
+      event.id =  doc.id
+      return event
+    })
+    res.send(matches)
+  })
+  .catch(err => res.status(500).send('Could not find event'))
 }
